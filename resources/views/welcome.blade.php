fc7f1d5c8814f8cb9cd2217a48498287319b4866
zhenia1698 <los.zhenia1698@yandex.ru>
2018-11-20 23:16:56 +0300

    #15 Counter API Google Maps(8)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,6 +67,7 @@
     </head>
     <body>
 	    {!! $map['html'] !!}
+		<div class="flex-center position-ref full-height">
 		@if (Route::has('login'))
             <div class="top-right links">
                 @auth
@@ -77,5 +78,6 @@
                 @endauth
             </div>
         @endif
+		</div>
     </body>
 </html>
